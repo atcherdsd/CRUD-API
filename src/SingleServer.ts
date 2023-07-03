230f29ce547c3de76fabce3486bb1860d1a641b0
atcherdsd <94963661+atcherdsd@users.noreply.github.com>
2023-07-04 00:32:50 +0300

    feat: add all methods

--- src/SingleServer.ts
+++ src/SingleServer.ts
@@ -1,10 +1,11 @@
 import http from 'http';
+import { v4 } from 'uuid';
 import { IUser, Methods } from "./types/interfaces";
 import { MethodsNames, StatusCodes, StatusMessages } from './types/enums';
 import { PORT, endpoint } from './utilities/utils';
 
 export class SingleServer {
-  public usersBase: IUser[] = [];
+  public usersDataBase: IUser[] = [];
   public port = PORT;
 
   public server = http.createServer((request, response) => {
@@ -12,6 +13,15 @@ export class SingleServer {
       switch (request.method as Methods) {
         case MethodsNames.GET:
           this.makeGet(request, response);
+          break;
+        case MethodsNames.POST:
+          this.makePost(request, response);
+          break;
+        case MethodsNames.PUT:
+          this.makePut(request, response);
+          break;
+        case MethodsNames.DELETE:
+          this.makeDelete(request, response);
           break;
         default:
           throw new Error();
@@ -30,7 +40,7 @@ export class SingleServer {
   }
 
   public close() {
-    this.usersBase = [];
+    this.usersDataBase = [];
     this.server.close();
   }
 
@@ -47,7 +57,7 @@ export class SingleServer {
       const userId = url.split('/').slice(-1).toString();
 
       if (userId && this.validateIdType(userId)) {
-        const requestedUser = this.usersBase.find((user) => user.id === userId);
+        const requestedUser = this.usersDataBase.find((user) => user.id === userId);
 
         if (requestedUser) {
           cb(requestedUser);
@@ -75,6 +85,32 @@ export class SingleServer {
     );
   }
 
+  private verifyRequestData(
+    data: Buffer[], 
+    response: http.ServerResponse, 
+    callback: (body: IUser) => void
+  ) {
+    try {
+      const requestBody = JSON.parse(Buffer.concat(data).toString());
+      if ('username' in requestBody 
+        && 'age' in requestBody 
+        && 'hobbies' in requestBody) {
+        callback(requestBody);
+        return;
+      }
+      this.getErrorResponse(
+        response, 
+        StatusCodes.BAD_REQUEST, 
+        StatusMessages.BadRequest
+      );
+    } catch {
+      this.getErrorResponse(
+        response, StatusCodes.INTERNAL_SERVER_ERROR, 
+        StatusMessages.ServerError
+      );
+    }
+  }
+
   private getSuccessResponse(
     response: http.ServerResponse<http.IncomingMessage>, 
     statusCode: StatusCodes, 
@@ -94,12 +130,15 @@ export class SingleServer {
     response.end();
   }
 
-  public makeGet(request: http.IncomingMessage, response: http.ServerResponse) {
+  public makeGet(
+    request: http.IncomingMessage, 
+    response: http.ServerResponse
+  ): void {
     if (request.url === endpoint) {
       this.getSuccessResponse(
         response, 
         StatusCodes.OK, 
-        JSON.stringify(this.usersBase)
+        JSON.stringify(this.usersDataBase)
       );
       return;
     }
@@ -110,5 +149,77 @@ export class SingleServer {
         JSON.stringify(requestedUser)
       )
     );
+  }
+
+  public makePost(
+    request: http.IncomingMessage, 
+    response: http.ServerResponse
+  ): void {
+    if (request.url === endpoint) {
+      const dataFromRequest: Buffer[] = [];
+
+      request
+        .on('data', (chunk: Buffer) => {
+          dataFromRequest.push(chunk);
+        })
+        .on('end', () => {
+          this.verifyRequestData(dataFromRequest, response, (body) => {
+            const newUser = { ...body, id: v4() };
+            this.getSuccessResponse(
+              response, 
+              StatusCodes.OK, 
+              JSON.stringify(newUser)
+            );
+            this.usersDataBase.push(newUser);
+          });
+        });
+      return;
+    }
+    this.getErrorResponse(
+      response, 
+      StatusCodes.NOT_FOUND, 
+      StatusMessages.NotFound
+    );
+  }
+
+  public makePut(
+    request: http.IncomingMessage, 
+    response: http.ServerResponse
+  ): void {
+    this.verifyEndpointExist(request, response, ({ id: userId }: IUser) => {
+      const dataFromRequest: Buffer[] = [];
+      request
+        .on('data', (chunk: Buffer) => {
+          dataFromRequest.push(chunk);
+        })
+        .on('end', () => {
+          this.verifyRequestData(dataFromRequest, response, (body) => {
+            const updatedUserData = { ...body, id: userId };
+            this.usersDataBase = this.usersDataBase
+              .map((user) => (user.id === userId ? updatedUserData : user));
+
+            this.getSuccessResponse(
+              response, 
+              StatusCodes.OK, 
+              JSON.stringify(updatedUserData)
+            );
+          });
+        });
+    });
+  }
+
+  public makeDelete(
+    request: http.IncomingMessage,
+    response: http.ServerResponse
+  ): void {
+    this.verifyEndpointExist(request, response, (requestedUser: IUser) => {
+      this.usersDataBase = this.usersDataBase
+        .filter((user) => user.id !== requestedUser.id);
+
+      this.getSuccessResponse(
+        response, 
+        StatusCodes.NO_CONTENT
+      );
+    });
   }
 }
